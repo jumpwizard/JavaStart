fd57daf545ccbc0bb597da252a94a3136e8d1bb3
jumpwizard <58380133+jumpwizard@users.noreply.github.com>
2021-12-19 22:52:04 +0800

    Add files via upload

---
+++ week3/ch1/Animal.java
@@ -0,0 +1,10 @@
+package com.student.override_;
+
+public class Animal {
+    public void cry() {
+        System.out.println("Animal cry");
+    }
+    public Object m1() {
+        return null;
+    }
+}
