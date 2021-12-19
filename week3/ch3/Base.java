cbed611fb72088901d7ba8782808b29abbdcee97
jumpwizard <58380133+jumpwizard@users.noreply.github.com>
2021-12-19 22:57:10 +0800

    Add files via upload

---
+++ week3/ch3/Base.java
@@ -0,0 +1,15 @@
+package com.student.extend_.detail_;
+
+public class Base {
+    public int n1 = 100;
+    protected int n2 = 200;
+    int n3 = 300;
+    private int  n4 = 400;
+
+//    public Base() {
+//        System.out.println("Base 无参");
+//    }
+    public Base(int n) {
+        System.out.println("Base 有参");
+    }
+}
---
+++ week3/ch3/Sub.java
@@ -0,0 +1,8 @@
+package com.student.extend_.detail_;
+
+public class Sub extends Base{
+    public Sub() {
+        super(24);
+        System.out.println("Sub");
+    }
+}
